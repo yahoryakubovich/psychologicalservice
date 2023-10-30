cfea14665b0da2ded64b742c663b84979ce07086
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-10-30 21:14:58 +0300

    Реализованы представления для создания, чтения, обновления и удаления статей в приложении 'articles'. Добавлены URL-маршруты и шаблоны для приложения 'articles'

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)  # Максимальная длина заголовка статьи
+    content = models.TextField()  # Текст статьи
+    pub_date = models.DateTimeField('date published')  # Дата публикации статьи
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Связь с моделью пользователя Django
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_date']  # Сортировка статей по убыванию даты публикации
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.utils import timezone
+from .models import Article
 
-# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/article_list.html'
+    context_object_name = 'articles'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/article_detail.html'
+    context_object_name = 'article'
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'articles/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.pub_date = timezone.now()  # Устанавливаем текущую дату и время
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('articles:article_list')
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'articles/article_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('articles:article_list')
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'articles/article_confirm_delete.html'
+    success_url = reverse_lazy('articles:article_list')
